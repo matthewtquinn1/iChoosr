37cd394e5c00342bc5890c6acd28b4ebd6e498e0
Matthew Quinn <matthewtquinn1@gmail.com>
2020-01-22 00:41:42 +0100

    Adding example test

--- IChoosr/IChoosr.BL.Tests/Service/Camera/CameraTests.cs
+++ IChoosr/IChoosr.BL.Tests/Service/Camera/CameraTests.cs
@@ -1,3 +1,10 @@
+using System.IO;
+using System.Linq;
+using FluentAssertions;
+using IChoosr.BL.Interface.Camera;
+using IChoosr.BL.Service.Camera;
+using IChoosr.DA;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace IChoosr.BL.Tests.Service.Camera
@@ -5,16 +12,27 @@ namespace IChoosr.BL.Tests.Service.Camera
     [TestFixture]
     public class CameraTests
     {
+        private ICameraRepository _repo;
+        private ICameraService _service;
+
         [SetUp]
         public void Setup()
         {
+            _repo = Substitute.For<ICameraRepository>();
 
+            _service = new CameraService(_repo);
         }
 
+        // TODO: Won't pass yet, example test of style
         [Test]
-        public void FirstTest_DoNothing()
+        public void WhenCallingGetCameras_ThenCamerasShouldBeReturned()
         {
+            // TODO: Setup test data
+            _repo.GetCameraCsv().Returns(new StreamReader(""));
 
+            var cameras = _service.GetCameras();
+
+            cameras.First().Id.Should().Be("1");
         }
       }
 }
